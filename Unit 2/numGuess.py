87577a05b5d81cbf585b65177157bd2f23dad642
SaltyMelonYT <jonnyb1109@gmail.com>
2025-09-11 13:26:42 -0400

    Updated trust

--- Unit 2/numGuess.py
+++ Unit 2/numGuess.py
@@ -1,19 +1,18 @@
+# 2.9
 import random
 attempts=0
 play=input("Would you like to play? y/N ")
 num=random.randint(1,20)
-print(num)
 while play.lower() == 'y':
     guess=int(input("What is your guess? "))
     if guess > 20 or guess < 1:
         print("please enter a valid number of 1-20")
     elif guess != num:
-        attempts+=1
+        attempts=attempts+1
         print("Wrong! Try again.")
-        print(attempts)
     else:
         print("Correct! You win!")
-        print(f"You attempted {attempts} times!")
+        print(f"You attempted {attempts+1} times!")
         play=input("Play again? y/N ")
         num=random.randint
         attempts=0
